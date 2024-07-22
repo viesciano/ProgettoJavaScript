383779b64a64a698dae6e6021d575c3673ff0db5
simonecamilli <simoneandreacamilli@gmail.com>
2024-07-22 11:10:31 +0200

    buttons in js added

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,23 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
     const display = document.getElementById("display-1");
+    const primaryOperationsColumn = document.getElementById("primary-operations-column");
+    const secondaryOperationsColumn = document.getElementById("secondary-operations-column");
     const mainContainer = document.getElementById("main-container");
     let sum = 0;
     let countdownInterval;
-  
+
     function formatTime(seconds) {
         let mins = Math.floor(seconds / 60);
         let secs = seconds % 60;
         return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
     }
-  
+
     function updateDisplay() {
         display.textContent = formatTime(sum);
     }
-  
+
+    function createElement(type, className, id, textContent) {
+        const element = document.createElement(type);
+        if (className) element.className = className;
+        if (id) element.id = id;
+        if (textContent) element.textContent = textContent;
+        return element;
+    }
+
+    function addButtons() {
+        const minusButton = createElement("button", "basic-but", "minus-button", "-");
+        const plusButton = createElement("button", "basic-but", "plus-button", "+");
+        primaryOperationsColumn.appendChild(minusButton);
+        primaryOperationsColumn.appendChild(plusButton);
+
+        const startButton = createElement("button", "oper-but", "start-button", "Start");
+        const stopButton = createElement("button", "oper-but", "stop-button", "Stop");
+        const resetButton = createElement("button", "oper-but", "reset-button", "Reset");
+        secondaryOperationsColumn.appendChild(startButton);
+        secondaryOperationsColumn.appendChild(stopButton);
+        secondaryOperationsColumn.appendChild(resetButton);
+    }
+
     mainContainer.addEventListener("click", function (event) {
         const action = event.target.getAttribute("id");
-  
-        switch(action) {
+
+        switch (action) {
             case "minus-button":
                 if (sum > 0) {
                     sum -= 60;
@@ -45,7 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
             case "stop-button":
                 clearInterval(countdownInterval);
                 break;
-                
+
             case "reset-button":
                 clearInterval(countdownInterval);
                 sum = 0;
@@ -53,6 +77,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 break;
         }
     });
-  
+
+    addButtons();
     updateDisplay();
-  });
+});
